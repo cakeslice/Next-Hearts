c3062caa8e9311d49b3fcdc20ed837bcf60ca38f
José Guerreiro <jose-guerreiro@live.com.pt>
2023-09-18 15:03:15 +0200

    Preload all card images so they show up instantly during gameplay

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -2,6 +2,7 @@ import { description, title } from 'config'
 import Head from 'next/head'
 
 import clsx from 'clsx'
+import { cardsList } from 'models/card'
 import styles from './PageWrapper.module.scss'
 
 export default function PageWrapper({
@@ -25,6 +26,11 @@ export default function PageWrapper({
 					name='viewport'
 					content='width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0'
 				/>
+
+				{/* Preload all card images so they show up instantly during gameplay */}
+				{cardsList.map((c) => (
+					<link key={c} rel='preload' as='image' href={`/assets/cards/${c}.svg`} />
+				))}
 			</Head>
 
 			<div className={clsx(styles.Container, 'flex justify-center items-center')}>
