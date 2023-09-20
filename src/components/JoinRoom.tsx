5229a06de98e583dce9699a4e99f8b8a29fd02fd
José Guerreiro <jose-guerreiro@live.com.pt>
2023-09-20 09:03:55 +0200

    Add GitHub link

--- src/components/JoinRoom.tsx
+++ src/components/JoinRoom.tsx
@@ -4,7 +4,7 @@ import { useCallback, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import styles from './JoinRoom.module.scss'
 
-import { Button, Input, Modal, ModalContent, Spacer } from '@nextui-org/react'
+import { Button, Input, Link, Modal, ModalContent, Spacer, Tooltip } from '@nextui-org/react'
 import clsx from 'clsx'
 import { title } from 'config'
 import { request } from 'core/client/api'
@@ -12,7 +12,24 @@ import { Body, Response } from 'pages/api/join-game'
 import { modalProps } from 'utils/consts'
 import card2 from '../../public/assets/cards/ace_of_hearts.svg'
 import card1 from '../../public/assets/cards/queen_of_spades.svg'
+import githubIcon from '../../public/assets/github-mark.svg'
 import { ModalWrapper } from './ModalContent'
+
+const ForkMeButton = () => (
+	<div className=' self-end max-h-0 relative'>
+		<Tooltip
+			placement='right'
+			content='Fork me on GitHub'
+			classNames={{
+				base: 'py-2 px-4 shadow-xl text-black bg-gradient-to-br from-white to-neutral-400',
+			}}
+		>
+			<Link href='https://github.com/cakeslice/Next-Hearts' target='_blank'>
+				<Image width={20} height={20} alt='GitHub' src={githubIcon} />
+			</Link>
+		</Tooltip>
+	</div>
+)
 
 const Logo = () => {
 	return (
@@ -82,6 +99,8 @@ export const useJoinRoom = (
 						})}
 					>
 						<ModalWrapper>
+							<ForkMeButton />
+
 							<Spacer y={1} />
 
 							<Logo />
