8ad006dbac531b0543038286328566afdc7c7d81
José Guerreiro <jose-guerreiro@live.com.pt>
2024-02-27 10:18:06 +0100

    Improve default next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,20 @@
+const withPlugins = require('next-compose-plugins')
 const withExportImages = require('next-export-optimize-images')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true',
+})
 
 /** @type {import('next').NextConfig} */
-module.exports = withExportImages({
-	output: 'standalone', // undefined for default, 'export' for static sites, 'standalone' for custom server
-	reactStrictMode: true,
-	swcMinify: true,
-	transpilePackages: ['@uidotdev', '@heroicons'],
-	eslint: {
-		// TODO: Remove after fixing the eslint issue
-		ignoreDuringBuilds: true,
-	},
-})
+module.exports = withPlugins(
+	process.env.EXPORT === 'true' ? [withExportImages, withBundleAnalyzer] : [withBundleAnalyzer],
+	{
+		output: process.env.EXPORT ? 'export' : 'standalone', // undefined for default, 'export' for static sites, 'standalone' for custom server
+		reactStrictMode: true,
+		swcMinify: true,
+		transpilePackages: ['@uidotdev', '@heroicons'],
+		eslint: {
+			// TODO: Remove after fixing the eslint issue
+			ignoreDuringBuilds: true,
+		},
+	}
+)
